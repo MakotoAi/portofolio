92bc4e28a999665f43e3aaa350a7d1956ca08fd3
MakotoAi <hanifdrhm007@gmail.com>
2024-10-17 09:01:17 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/info', methods=['GET'])
+def get_info():
+    my_name = request.args.get('my_name')
+    print(my_name)
+    return jsonify({
+        'msg': 'GET Info'
+    })
+
+@app.route('/info', methods=['POST'])
+def post_info():
+    my_name = request.form['my_name']
+    print(my_name)
+    return jsonify({
+        'msg': 'POST Info'
+    })
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
